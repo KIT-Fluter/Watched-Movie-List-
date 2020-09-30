a973a0326a4c321171890fb2c8030a7536f228ca
km111010 <kohki.m111010@gmail.com>
2020-09-30 23:34:51 +0900

    [update]mainを変更(仮)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/header.dart';
+import 'widgets/footer.dart';
+
 void main() {
-  runApp(MyApp())
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: Header(),
+        body:  Center(child: Text("うんこ")),
+        bottomNavigationBar: Footer()
+      )
+    );
+  }
 }
